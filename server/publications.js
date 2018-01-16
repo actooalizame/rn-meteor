0e0258db48137c630ad6579b59398ca4b70c697f
actooalizame <gndalmedo@gmail.com>
2018-01-16 19:44:12 -0300

    new Cars collection added

--- server/publications.js
+++ server/publications.js
@@ -1,5 +1,10 @@
 import {Items} from '../lib/items';
+import {Cars} from '../lib/cars';
 
 Meteor.publish('items', function(){
 	return Items.find();
+});
+
+Meteor.publish('cars', function(){
+ return Cars.find();
 });
--- server/seed.js
+++ server/seed.js
@@ -1,5 +1,6 @@
 import {Meteor} from 'meteor/meteor';
 import {Items} from '../lib/items';
+import {Cars} from '../lib/cars';
 
 Meteor.startup(function(){
 	if (Items.find().count() === 0) {
@@ -14,5 +15,23 @@ Meteor.startup(function(){
       timestamp += 1; // ensure unique timestamp
       i += 1;
     }
+  }
+  if (Cars.find().count() === 0) {
+    let timestamp = (new Date()).getTime();
+      Cars.insert({
+        name: 'Land Rover Discovery Sport',
+        price: '22970000',
+        year: '2015',
+        km: '50000',
+        brand: 'Land Rover',
+        model: 'Discovery Sport HSE 2.0 Si4',
+        equipment: 'Ultra Full - Automático',
+        color: 'Negro Santorini',
+        interior: 'Cuero Beige - Aluminio',
+        roof: 'Sunroof Panorámico',
+        security: '12 Airbags - Frenos ABS',
+        createdAt: new Date(timestamp)
+      });
+      
   }
 });
